d0a0ba82fb78c4111642888633bbe1c8486d9af7
ChuchuComete <160599605+ChuchuComete@users.noreply.github.com>
2025-01-08 03:32:00 +0100

    Update songs.js

--- src/js/data/2024-01-31/misc/songs.js
+++ src/js/data/2024-01-31/misc/songs.js
@@ -1,8 +1,8 @@
 
 export const songs = [
 	{
-    name: "1.0. Major - Kokoroe by ROAD OF MAJOR",
-    videoId : "https://naedist.animemusicquiz.com/7ritrj.webm",
+    name: "1.0. Angel Beats! - My Soul,Your Beats! by LiSA",
+    videoId : "https://nawdist.animemusicquiz.com//swjb4r.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -11,8 +11,8 @@ export const songs = [
     }
 },
 	{
-    name: "2.0. Major - Saraba Aoki Omokage by ROAD OF MAJOR",
-    videoId : "https://naedist.animemusicquiz.com/g2dr6a.webm",
+    name: "2.0. Angel Beats! - Ichiban no Takaramono by LiSA",
+    videoId : "https://nawdist.animemusicquiz.com//mrxgqk.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -21,8 +21,8 @@ export const songs = [
     }
 },
 	{
-    name: "3.0. Major - PLAY THE GAME by ROAD OF MAJOR",
-    videoId : "https://naedist.animemusicquiz.com/m02fdw.webm",
+    name: "3.0. Angel Beats! - Thousand Enemies by Girls Dead Monster (LiSA)",
+    videoId : "https://nawdist.animemusicquiz.com/gfogq8.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -31,8 +31,8 @@ export const songs = [
     }
 },
 	{
-    name: "4.0. Major - RISE by Kohei Otomo",
-    videoId : "https://naedist.animemusicquiz.com/zijh3n.webm",
+    name: "4.0. Fate/Zero - oath sign by LiSA",
+    videoId : "https://nawdist.animemusicquiz.com//qgy2zu.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -41,8 +41,8 @@ export const songs = [
     }
 },
 	{
-    name: "5.0. Major - Hey! Hey! Alright by Scha Dara Parr+Kaela Kimura",
-    videoId : "https://naedist.animemusicquiz.com/9bbkb2.webm",
+    name: "5.0. Sword Art Online - crossing field by LiSA",
+    videoId : "https://nawdist.animemusicquiz.com/35s57l.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -51,8 +51,8 @@ export const songs = [
     }
 },
 	{
-    name: "6.0. Major - Kokoroe by TRIPLANE",
-    videoId : "https://naedist.animemusicquiz.com/t5x03q.webm",
+    name: "6.0. Day Break Illusion - träumerei by LiSA",
+    videoId : "https://nawdist.animemusicquiz.com/jil0vz.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -61,8 +61,8 @@ export const songs = [
     }
 },
 	{
-    name: "7.0. Major - Step by Beni Arashiro",
-    videoId : "https://naedist.animemusicquiz.com/930dwb.webm",
+    name: "7.0. The Irregular at Magic High School - Rising Hope by LiSA",
+    videoId : "https://nawdist.animemusicquiz.com//5p0wyp.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -71,8 +71,8 @@ export const songs = [
     }
 },
 	{
-    name: "8.0. Major - Faraway by PARADISE GO!! GO!!",
-    videoId : "https://naedist.animemusicquiz.com/43ba03.webm",
+    name: "8.0. Mekakucity Actors - Headphone Actor by Jin ft. LiSA",
+    videoId : "https://nawdist.animemusicquiz.com//zde4lq.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -81,8 +81,8 @@ export const songs = [
     }
 },
 	{
-    name: "9.0. Major - WONDERLAND by MAY",
-    videoId : "https://naedist.animemusicquiz.com/1vkyay.webm",
+    name: "9.0. Mekakucity Actors - Yuukei Yesterday by Jin ft. LiSA",
+    videoId : "https://nawdist.animemusicquiz.com/6l333m.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -91,8 +91,8 @@ export const songs = [
     }
 },
 	{
-    name: "10.0. Major - Shoboi Kao Sunna yo Baby by The Loose Dogs",
-    videoId : "https://naedist.animemusicquiz.com/m1n8gd.webm",
+    name: "10.0. Sword Art Online II - No More Time Machine by LiSA",
+    videoId : "https://nawdist.animemusicquiz.com/xl3s4n.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -101,8 +101,8 @@ export const songs = [
     }
 },
 	{
-    name: "11.0. Major - Strike Party!!! by BeForU",
-    videoId : "https://naedist.animemusicquiz.com/kc1g6f.webm",
+    name: "11.0. Sword Art Online II - Shirushi by LiSA",
+    videoId : "https://nawdist.animemusicquiz.com//0v17fr.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -111,8 +111,8 @@ export const songs = [
     }
 },
 	{
-    name: "12.0. Major - Yoru ni Nareba by The Loose Dogs",
-    videoId : "https://naedist.animemusicquiz.com/raw85d.webm",
+    name: "12.0. Fate/stay night: Unlimited Blade Works - THIS ILLUSION by LiSA",
+    videoId : "https://nawdist.animemusicquiz.com//3bk1mq.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -121,8 +121,8 @@ export const songs = [
     }
 },
 	{
-    name: "13.0. Major - ONE DAY by The Loose Dogs",
-    videoId : "https://naedist.animemusicquiz.com/m80ouq.webm",
+    name: "13.0. Nisekoi: - Rally Go Round by LiSA",
+    videoId : "https://nawdist.animemusicquiz.com/x9m10f.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -131,8 +131,8 @@ export const songs = [
     }
 },
 	{
-    name: "14.0. Major - Ame Nochi Niji-iro by Showtaro Morikubo×The Loose Dogs feat. Maki Ohguro",
-    videoId : "https://naedist.animemusicquiz.com/euyrpq.webm",
+    name: "14.0. Qualidea Code - Brave Freak Out by LiSA",
+    videoId : "https://nawdist.animemusicquiz.com/umgu9c.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -141,8 +141,8 @@ export const songs = [
     }
 },
 	{
-    name: "15.0. Major - Stay with me by Hitomi Shimatani",
-    videoId : "https://naedist.animemusicquiz.com/wfynr6.webm",
+    name: "15.0. Qualidea Code - AxxxiS by LiSA",
+    videoId : "https://nawdist.animemusicquiz.com/39gx31.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -151,8 +151,8 @@ export const songs = [
     }
 },
 	{
-    name: "16.0. Major - Jibun Color by Yu Nakamura",
-    videoId : "https://naedist.animemusicquiz.com/vq017m.webm",
+    name: "16.0. Sword Art Online the Movie: Ordinal Scale - Catch the Moment by LiSA",
+    videoId : "https://nawdist.animemusicquiz.com/xgsnd3.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -161,8 +161,8 @@ export const songs = [
     }
 },
 	{
-    name: "17.0. Major - Twilight Star by Megamasso",
-    videoId : "https://naedist.animemusicquiz.com/riybuh.webm",
+    name: "17.0. My Hero Academia - Datte Atashi no Hero. by LiSA",
+    videoId : "https://nawdist.animemusicquiz.com/pcvhkl.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -171,8 +171,8 @@ export const songs = [
     }
 },
 	{
-    name: "18.0. Major - Zutto Mae kara by French Kiss",
-    videoId : "https://naedist.animemusicquiz.com/h13qit.webm",
+    name: "18.0. Fate/Apocrypha - ASH by LiSA",
+    videoId : "https://nawdist.animemusicquiz.com//n2qrvu.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -181,8 +181,8 @@ export const songs = [
     }
 },
 	{
-    name: "19.0. Major: Yuujou no Winning Shot - Tsubasa by Remioromen",
-    videoId : "https://naedist.animemusicquiz.com/1cdpxr.webm",
+    name: "19.0. Sword Art Online: Alicization - ADAMAS by LiSA",
+    videoId : "https://nawdist.animemusicquiz.com/pqcd4f.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -191,8 +191,8 @@ export const songs = [
     }
 },
 	{
-    name: "20.0. Major: Message - Kokoroe by Showtaro Morikubo",
-    videoId : "https://naedist.animemusicquiz.com/fuzt6a.webm",
+    name: "20.0. Mobile Suit Gundam Narrative - narrative by SawanoHiroyuki[nZk]:LiSA",
+    videoId : "https://nawdist.animemusicquiz.com/7s114b.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -201,8 +201,8 @@ export const songs = [
     }
 },
 	{
-    name: "21.0. Major: World Series-hen - Yume no Shunkan e - JUST DO IT by CLUTCHO",
-    videoId : "https://naedist.animemusicquiz.com/jxr4bl.webm",
+    name: "21.0. Demon Slayer: Kimetsu no Yaiba - Gurenge by LiSA",
+    videoId : "https://nawdist.animemusicquiz.com/ef2ntg.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -211,8 +211,8 @@ export const songs = [
     }
 },
 	{
-    name: "22.0. Major: World Series-hen - Yume no Shunkan e - Time capsule by CLUTCHO",
-    videoId : "https://naedist.animemusicquiz.com/wocsns.webm",
+    name: "22.0. Demon Slayer: Kimetsu no Yaiba - from the edge by FictionJunction feat. LiSA",
+    videoId : "https://nawdist.animemusicquiz.com/z53lx5.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -221,8 +221,8 @@ export const songs = [
     }
 },
 	{
-    name: "23.0. Major 2nd - Koete Ike by Kyuuso Nekokami",
-    videoId : "https://naedist.animemusicquiz.com/241xwi.webm",
+    name: "23.0. Sword Art Online: Alicization - War of Underworld - unlasting by LiSA",
+    videoId : "https://nawdist.animemusicquiz.com/cywd9d.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -231,8 +231,8 @@ export const songs = [
     }
 },
 	{
-    name: "24.0. Major 2nd - Dream Catcher by Berry Goodman",
-    videoId : "https://naedist.animemusicquiz.com/2a75ft.webm",
+    name: "24.0. Ani × Para: Anata no Hero wa Dare desu ka - Harebutai by LiSA",
+    videoId : "https://nawdist.animemusicquiz.com/qsqmp3.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -241,8 +241,8 @@ export const songs = [
     }
 },
 	{
-    name: "25.0. Major 2nd - Pride by Yuu Takahashi",
-    videoId : "https://naedist.animemusicquiz.com/vp908v.webm",
+    name: "25.0. Back Arrow - dawn by LiSA",
+    videoId : "https://nawdist.animemusicquiz.com//uvvcdt.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -251,8 +251,8 @@ export const songs = [
     }
 },
 	{
-    name: "26.0. Major 2nd - SAIREN by Reol",
-    videoId : "https://naedist.animemusicquiz.com/0z2usy.webm",
+    name: "26.0. Demon Slayer: Kimetsu no Yaiba The Movie - Mugen Train - Homura by LiSA",
+    videoId : "https://nawdist.animemusicquiz.com//w0f6i5.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -261,8 +261,8 @@ export const songs = [
     }
 },
 	{
-    name: "27.0. Major 2nd - Answer by Leo Ieiri",
-    videoId : "https://naedist.animemusicquiz.com/m90ypk.webm",
+    name: "27.0. Sword Art Online the Movie: Progressive - Aria of a Starless Night - Yuke by LiSA",
+    videoId : "https://nawdist.animemusicquiz.com//qrq9zd.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -271,8 +271,8 @@ export const songs = [
     }
 },
 	{
-    name: "28.0. Major 2nd - Shiroi Doro by Mone Kamishiraishi",
-    videoId : "https://naedist.animemusicquiz.com/cj2fvx.webm",
+    name: "28.0. Demon Slayer: Kimetsu no Yaiba Mugen Train Arc - Akeboshi by LiSA",
+    videoId : "https://nawdist.animemusicquiz.com/t9p8wo.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -281,8 +281,58 @@ export const songs = [
     }
 },
 	{
-    name: "29.0. Major 2nd - One by SHE'S",
-    videoId : "https://naedist.animemusicquiz.com/v5rixm.webm",
+    name: "29.0. Demon Slayer: Kimetsu no Yaiba Mugen Train Arc - Shirogane by LiSA",
+    videoId : "https://nawdist.animemusicquiz.com//6r79u2.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "30.0. Demon Slayer: Kimetsu no Yaiba Mugen Train Arc - Homura by LiSA",
+    videoId : "https://nawdist.animemusicquiz.com/w0f6i5.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "31.0. The Irregular at Magic High School Season 3 - Shouted Serenade by LiSA",
+    videoId : "https://nawdist.animemusicquiz.com/t1q2yp.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "32.0. NieR:Automata Ver1.1a - Black Box by LiSA",
+    videoId : "https://nawdist.animemusicquiz.com/mrie4l.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "33.0. Solo Leveling Season 2: Arise from the Shadow - ReawakeR by LiSA feat. Felix of Stray Kids",
+    videoId : "https://nawdist.animemusicquiz.com/zbxil2.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "34.0. Shangri-La Frontier - QUEEN by LiSA",
+    videoId : "https://nawdist.animemusicquiz.com/973phv.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -290,8 +340,8 @@ export const songs = [
         ]
     }
 },{
-    name: "30.0. Major 2nd - IDENTITY by Ame no Parade",
-    videoId : "https://naedist.animemusicquiz.com/bg7ntd.webm",
+    name: "35.0. Spider-Man: Across the Spider-Verse - REALiZE by LiSA",
+    videoId : "https://youtu.be/-A0ICz6rVvU?si=ZL80VT-DojmQHRHJ",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
